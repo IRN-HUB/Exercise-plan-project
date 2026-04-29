71f8e072c287d478c33109d3e1030b3ed76ce4e2
IRN-HUB <2019524221@qq.com>
2026-04-30 01:46:01 +0800

    Update base path to /exercise/

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Exercise-plan-project/',
+  base: '/exercise/',
   plugins: [react()],
   server: {
     port: 3000,
